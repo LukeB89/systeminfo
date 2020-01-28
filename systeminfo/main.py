e9d152d21f1419cc65eb1234920a8a4fd9f48d0b
Luke Byrne <lukebyrne89@gmail.com>
2020-01-28 10:06:58 +0000

    System Info First Build

--- systeminfo/main.py
+++ systeminfo/main.py
@@ -1,12 +1,32 @@
 '''
-Created on 22 Jan 2017
+Created on 28 Jan 2017
 
-@author: aonghus
+@author: Luke
 '''
 import platform
+import socket
+import multiprocessing
+import timeit
+import math
+from psutil import virtual_memory
+
+ 
 
 def main():
-    print("The platform is", platform.platform())
+    mem = virtual_memory()
+    print("The Name of the computer is:", platform.node())
+    print("The OS Name is:",platform.system())
+    print("The OS Version is:", platform.version())
+    print("The number of Cores is:",multiprocessing.cpu_count())
+    print("The amount of memory in the computer is:",((mem.total/(1024**3))))
+    print("The IP address is:",socket.gethostbyname(socket.gethostname()))
+    t1 = timeit.default_timer()
+    math.factorial(1000)
+    t2 = timeit.default_timer()
+    time = round((t2-t1)*1000000, 3)
+    print("Time Taken: "+str(time)+" microseconds")
+    
+
     return
 
 
